0fac3e10e5db79be570d395f2190f27fdd204e47
karthikbhukya2005 <karthikbhukya667@gmail.com>
2026-03-06 17:21:50 +0530

    Update app-layout.jsx

--- src/layout/app-layout.jsx
+++ src/layout/app-layout.jsx
@@ -11,7 +11,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Made with 🖤 by Ajay
+        Made with 🖤 by Karthik
       </div>
     </div>
   );
